edc2a401f607cbccf3a88883be9356049217191c
yangsgit <liyanghehe@gmail.com>
2019-10-08 13:21:49 -0400

    add day 6

--- 6Day.playground/Contents.swift
+++ 6Day.playground/Contents.swift
@@ -1,3 +1,111 @@
 import UIKit
 
 var str = "Hello, playground"
+
+let driving = {
+    print("I'm driving in my car")
+}
+
+driving()
+
+let driving2 = { (place: String) -> Void in
+    print("I'm going to \(place) in my car")
+}
+driving2("London")
+
+let drivingWithReturn = { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+let message = drivingWithReturn("London")
+print(message)
+
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+
+travel(action: driving)
+
+travel {
+    print("I'm driving in my car")
+}
+
+func travel(action: (String) -> Void) {
+    print("I'm getting ready to go.")
+    action("London")
+    print("I arrived!")
+}
+
+travel { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+
+
+func travel(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
+}
+
+travel { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+travel { place -> String in
+    return "I'm going to \(place) in my car"
+}
+
+travel { place in
+    return "I'm going to \(place) in my car"
+}
+
+//As the closure only has one line of code that must be the one that returns the value, so Swift lets us remove the return keyword too:
+travel { place in
+    "I'm going to \(place) in my car"
+}
+
+travel {
+    "I'm going to \($0) in my car"
+}
+
+
+func travel(action: (String, Int) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London", 60)
+    print(description)
+    print("I arrived!")
+}
+
+travel {
+    "I'm going to \($0) at \($1) miles per hour."
+}
+
+func travel() -> (String) -> Void {
+    return {
+        print("I'm going to \($0)")
+    }
+}
+
+let result = travel()
+result("London")
+
+let result2 = travel()("London")
+
+func travelcapturing() -> (String) -> Void {
+    var counter = 1
+
+    return {
+        print("\(counter). I'm going to \($0)")
+        counter += 1
+    }
+}
+
+let result3 = travelcapturing()
+
+
+result3("London")
+result3("London")
+result3("London")
